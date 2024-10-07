3a44a6780d29bc737134f5e93f541c95a25c4f16
withcenterdev5 <withcenter.dev5@gmail.com>
2024-10-07 15:19:56 +0800

    from flutterflow

--- super-library-5mglde/lib/custom_code/widgets/chat_room_list_view.dart
+++ super-library-5mglde/lib/custom_code/widgets/chat_room_list_view.dart
@@ -1,7 +1,4 @@
 // Automatic FlutterFlow imports
-import 'package:firebase_database/firebase_database.dart';
-import 'package:super_library/custom_code/actions/super_library.dart';
-
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'index.dart'; // Imports other custom widgets
@@ -9,6 +6,9 @@ import '/custom_code/actions/index.dart'; // Imports custom actions
 import 'package:flutter/material.dart';
 // Begin custom widget code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+import 'package:firebase_database/firebase_database.dart';
+import 'package:super_library/custom_code/actions/super_library.dart';
 
 class ChatRoomListView extends StatefulWidget {
   const ChatRoomListView({
--- super-library-5mglde/lib/custom_code/widgets/index.dart
+++ super-library-5mglde/lib/custom_code/widgets/index.dart
@@ -2,3 +2,4 @@ export 'chat_message_list_view.dart' show ChatMessageListView;
 export 'user_list_view.dart' show UserListView;
 export 'chat_bubble.dart' show ChatBubble;
 export 'chat_room_list_view.dart' show ChatRoomListView;
+export 'chat_room_create_screen.dart' show ChatRoomCreateScreen;
