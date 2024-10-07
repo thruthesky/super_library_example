e8815001dd271ea9af7ac1a9364ce462506f9434
Christian Bucal (Dev2) <withcenter.dev2@gmail.com>
2024-10-07 13:31:43 +0800

    added chat bubble

---
+++ super-library-5mglde/lib/custom_code/widgets/chat_bubble.dart
@@ -0,0 +1,29 @@
+// Automatic FlutterFlow imports
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'index.dart'; // Imports other custom widgets
+import '/custom_code/actions/index.dart'; // Imports custom actions
+import 'package:flutter/material.dart';
+// Begin custom widget code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+class ChatBubble extends StatefulWidget {
+  const ChatBubble({
+    super.key,
+    this.width,
+    this.height,
+  });
+
+  final double? width;
+  final double? height;
+
+  @override
+  State<ChatBubble> createState() => _ChatBubbleState();
+}
+
+class _ChatBubbleState extends State<ChatBubble> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- super-library-5mglde/lib/custom_code/widgets/index.dart
+++ super-library-5mglde/lib/custom_code/widgets/index.dart
@@ -1,2 +1,3 @@
 export 'chat_message_list_view.dart' show ChatMessageListView;
 export 'user_list_view.dart' show UserListView;
+export 'chat_bubble.dart' show ChatBubble;
